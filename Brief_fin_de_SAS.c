c3a058de1851d6db4af3c7a8531026de16e2bc4d
kerkazou <kerkazou.zakaria@gmail.com>
2021-11-22 17:05:29 +0100

    fin de travai du jour 1

--- Brief_fin_de_SAS.c
+++ Brief_fin_de_SAS.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include <windows.h>
+#define Nbr_clients 200
+
 int choix;
+char CIN[10], Nom[10], Prenom[10];
+double Montant;
+typedef struct {
+        char CIN[10];
+        char Nom[10];
+        char Prenom[10];
+        double Montant;
+    }compt;  
 
 void Affichage_Menu(){
     printf("\t \t Bienvenu sur la bonque :\n \n \n");
@@ -7,7 +18,8 @@ void Affichage_Menu(){
     printf("2) Introduire plusieurs comptes bancaires :\n \n");
     printf("3) Opérations :\n \n");
     printf("4) Affichage :\n \n");
-    printf("5) Quitter l’application :\n \n");
+    printf("5) Fidélisation :\n \n");
+    printf("6) Quitter l’application :\n \n \n");
 }
 
 int choisir(int choix){
@@ -16,39 +28,37 @@ int choisir(int choix){
     return choix;
 }
 
-void Introduire_un_compte_bancaire(char Nom[], char Prenom[], char CIN[], float Montant){
-    struct compt {
-        char *CIN;
-        char *Nom;
-        char *Prenom;
-        float *Montant;
-    }    
+char Introduire_un_compte_bancaire(char CIN[10], char Nom[10], char Prenom[10], double Montant){
+    system("cls");
+    compt info;
+    printf("Entrer les donnee suivent : \n \n \n");
+    printf("CIN : ");
+    scanf("%s",info.CIN);
+    printf("Nom : ");
+    scanf("%s",info.Nom);
+    printf("prenom : ");
+    scanf("%s",info.Prenom);
+    printf("Montant : ");
+    scanf("%lf",&info.Montant);
+    return CIN,Nom,Prenom,Montant;
 }
 
 
 
 int main()
 {
-    Affichage_Menu();
-    choisir(choix);
-    switch (choix)
-    {
-    case 1 :
-        break;
-    case 2 :
-        break;
-    case 3 :
-        break;
-    case 4 :
-        break;
-    case 5 :
-        break;
-    case 6 :
-        break;
-
-    default:
-        break;
+    do {
+        system("cls");
+        Affichage_Menu(); //affich le menu.
+        choix = choisir(choix);//choisir l'opération.
+        switch (choix){
+            case 1 :
+            Introduire_un_compte_bancaire(CIN, Nom, Prenom, Montant);//crée un compt.
+                break;
+            default:
+                break;
+        }
     }
-
+    while (choix<1 || choix>6);
     return 0;
 }
