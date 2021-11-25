eb2a38835ef312e720161717d3095d771c2dd128
kerkazou <kerkazou.zakaria@gmail.com>
2021-11-25 09:50:33 +0100

    le travail du jour 3

--- Brief_fin_de_SAS.c
+++ Brief_fin_de_SAS.c
@@ -4,157 +4,191 @@
 #include <string.h>
 #define Nbr_clients 2000
 
-int main() ;
-
 typedef struct {
         char CIN[30];
         char Nom[30];
         char Prenom[30];
         double Montant;
     }compte;
-    
 
-int choix,i,n;
-char CIN[30], Nom[30], Prenom[30];
-double Montant;
-int size=0;
-int compteur=0;
-//char Nbr_clients[];
 compte client[Nbr_clients]; //reserver un tableu
+int choix,size=0,i;
+int main();
 
-void Affichage_Menu(){
-    printf("\t \t \t Bienvenu sur la bonque :\n \n \n");
-    printf("\t 1) Introduire un compte bancaire :\n \n");
-    printf("\t 2) Introduire plusieurs comptes bancaires :\n \n");
-    printf("\t 3) Operations :\n \n");
-    printf("\t 4) Affichage :\n \n");
-    printf("\t 5) Fidelisation :\n \n");
-    printf("\t 6) Quitter l’application :\n \n \n");
+void Affichage_Menu(){     //affich le menu.
+    printf("\n\t ______________ Bienvenu sur la bonque _______________\n");
+    printf("\t | 1) Introduire un compte bancaire :                 |\n \n");
+    printf("\t | 2) Introduire plusieurs comptes bancaires :        |\n\n");
+    printf("\t | 3) Operations :                                    |\n\n");
+    printf("\t | 4) Affichage :                                     |\n\n");
+    printf("\t | 5) Fidelisation :                                  |\n\n");
+    printf("\t | 6) Quitter l’application :                         |\n\n");
+    printf("\t |____________________________________________________|\n\n\n\n\n");
 }
 
-int choisir(int choix){
+int choisir(int choix){   //choisir l'opération.
     printf("\t Votre choix : ");
     scanf("%d",&choix);
     printf("\n");
     return choix;
 }
 
-void Introduire_un_compte_bancaire(char CIN[30], char Nom[30], char Prenom[30], double Montant){
-    compte client[size];
+void Introduire_un_compte_bancaire(){
     system("cls");
-    size++;
-    compteur++;
-    printf("\t Entrer les donnee suivent : \n \n \n");
+    printf("\n \t Entrer les donnee suivent : \n \n \n");
     printf("CIN : ");
     scanf("%s",client[size].CIN);
-    printf("\n");
+    printf("\n \n");
     printf("Nom : ");
     scanf("%s",client[size].Nom);
-    printf("\n");
+    printf("\n \n");
     printf("prenom : ");
     scanf("%s",client[size].Prenom);
-    printf("\n");
+    printf("\n \n");
     printf("Montant : ");
     scanf("%lf",&client[size].Montant);
-    printf("\n");
-    printf("Votre compte bancaire a ete cree avec succes : \n");
-    printf("\n");
-    printf("CIN : %s   Nom : %s   Prenom : %s   Montant : %2.lf MAD" ,client[size].CIN, client[size].Nom , client[size].Prenom , client[size].Montant);
-    printf("\n");
+    printf("\n \n");
+    size++;
+    system("cls");
 }
 
-void Introduire_plusieurs_comptes_bancaires(char CIN[30], char Nom[30], char Prenom[30], double Montant){
-    compte client[size];
-    system("cls");
-    printf("Entre le nombre des clients : n = ");
-    scanf("%d",&n);
-    for (i=0;i<n;i++){
-    size++;
-    compteur++;
-    printf("Entrer les donnee suivent : \n \n \n");
-    printf("CIN : ");
-    scanf("%s",client[i].CIN);
-    printf("\n");
-    printf("Nom : ");
-    scanf("%s",client[i].Nom);
-    printf("\n");
-    printf("prenom : ");
-    scanf("%s",client[i].Prenom);
-    printf("\n");
-    printf("Montant : ");
-    scanf("%lf",&client[i].Montant);
-    printf("\n");
-    printf("Votre compte bancaire a ete cree avec succes : \n");
-    }
-    for (i=0;i<n;i++){
-        printf("CIN : %s   Nom : %s   Prenom : %s   Montant : %2.lf MAD" ,client[i].CIN, client[i].Nom , client[i].Prenom , client[i].Montant);
-        printf("\n");
-    }
+void Affichage(){
+		//printf("| CIN : %s | Nom : %s | Prenom : %s | Montant : %2.lf MAD |",client[i].CIN , client[i].Nom , client[i].Prenom , client[i].Montant);
+		printf("CIN : %s\n",client[i].CIN);
+		printf("Nom : %s\n",client[i].Nom);
+		printf("Prenom : %s\n",client[i].Prenom);
+		printf("Montant : %2.lf MAD\n",client[i].Montant);
+		printf("_________________________\n \n \n");
 }
 
 void retour_a_menu(){
-    int retour;
+	int retour;
     printf("1_ retour a menu : \n\n");
     printf("2_ Quitter l’application : \n\n");
-    scanf("%d",&retour);
-    if(retour==1)
-    {
+    //scanf("%d",&retour);
+    choix = choisir(choix);
+    if(choix==1){
        main();
     }
     else
-        printf("cette application n est pas exister maintenant.");
+    	system("cls");
+        printf("\n\n\n\n \t cette application n est pas exister maintenant.");
         exit(0);
 }
 
-void Retrait(){
-char R_CIN[30];
-int i;
-double R;
-printf("Entrez votre CIN");
-scanf("%s",R_CIN);
-    for (i = 0; i <= n; i++)
-    {
-        if (strcmp(R_CIN,client[i].CIN) == 0){  
-        printf("Entrez le solde : ");
-        scanf("%lf",&R);
-        client[i].Montant -= R;
-        printf("nouveau montant est %lf \n",client[i].Montant);
-        break;
-        }
+void Introduire_plusieurs_comptes_bancaires(){
+	int n_clients;
+    system("cls");
+    printf("\n Entre le nombre des clients : ");
+    scanf("%d",&n_clients);
+    for (i=0;i<n_clients;i++){
+        Introduire_un_compte_bancaire();
+    }
+	printf("Votre compte bancaire a ete cree avec succes : \n");
+	system("cls");
+	printf("Les comptes : \n\n");
+    for (i=0;i<n_clients;i++){
+        Affichage();
     }
 }
 
-void Depot(){
-char R_CIN[30];
-int i;
-double D;
-printf("Entrez votre CIN :");
-scanf("%s",R_CIN);
-    for (i = 0; i <= n; i++)
-    {
-        if (strcmp(R_CIN,client[i].CIN) == 0)
-        {  
-        printf("Entrez le solde : ");
-        scanf("%lf",&D);
-        client[i].Montant += D;
-        printf("nouveau montant est %lf \n", client[i].Montant);
-        break;
-        }
-    }
-}
-    
 void Operation(){
-    printf(" 1) Retrait: \n\n 2) Depot:\n");
-    choix = choisir(choix);
-    switch(choix)
-    {
-        case 1:
-            Depot();
-        break;
-        case 2:
-            Retrait();
-        break;
-    }
+	char R_CIN[30];
+	double r;
+	system("cls");
+	printf("\n \t Operation : \n \n \n");
+	printf(" 1) Retrait : \n\n 2) Depot :\n\n");
+	choix = choisir(choix);
+	switch(choix){
+		case 1: //Retrait.
+			printf("Entrez votre CIN : ");
+			scanf("%s",R_CIN);
+			for (i = 0; i <= size ; i++){
+		        if (strcmp(R_CIN,client[i].CIN) == 0){
+				    printf("Entrez le solde : ");
+				    scanf("%lf",&r);
+				    client[i].Montant -= r;
+				    printf("Votre montant maintenat est %lf\n",client[i].Montant);
+				    break;
+				}
+    		}
+		break;
+		case 2: //Depot.
+			printf("Entrez votre CIN : ");
+			scanf("%s",R_CIN);
+			for (i = 0; i <= size ; i++){
+		        if (strcmp(R_CIN,client[i].CIN) == 0){ 
+				printf("Entrez le solde : ");
+				scanf("%lf",&r);
+				client[i].Montant += r;
+				printf("Votre montant maintenat est %lf\n",client[i].Montant);
+				break;
+				}
+		break;
+			}
+	}
+}
+
+void Ascendent(){
+	compte tab_temp;
+	int i,j;
+	for(i=0 ; i<size ; i++){	
+		for(j=0 ; j<size-i-1 ; j++){
+			if (client[j].Montant > client[j + 1].Montant){
+				tab_temp=client[j];
+				client[j] =client[j + 1];
+				client[j + 1] = tab_temp;
+			}
+ 		}
+	}
+	printf("\n Les donners des comptes par ascendent est : \n\n  CIN  \t  Nom  \t  Prenom  \t  Montant \n");
+	for(i=0;i<size;i++){
+		printf("  %s \t %s \t %s \t %lf \n \n",client[i].Nom,client[i].Prenom,client[i].CIN,client[i].Montant);
+	}
+}
+
+void Descendent(){
+	compte tab_temp;
+	int i,j;
+	for(i=0 ; i<size ; i++){	
+		for(j=0 ; j<size-i-1 ; j++){
+			if (client[j].Montant < client[j + 1].Montant){
+				tab_temp=client[j];
+				client[j] =client[j + 1];
+				client[j + 1] = tab_temp;
+			}
+ 		}
+	}
+	printf(" \n Les donners des comptes par ascendent est : \n\n  CIN  \t  Nom  \t  Prenom  \t  Montant \n");
+	for(i=0;i<size;i++){
+		printf("  %s \t %s \t %s \t %lf \n \n",client[i].Nom,client[i].Prenom,client[i].CIN,client[i].Montant);
+	}
+}
+
+void Fidelisalton(){
+	printf("hhhhh fia lmoot dial n3as offf ");
+	compte tab_temp;
+	int i,j;
+	for(i=0 ; i<size ; i++){	
+		for(j=0 ; j<size-i-1 ; j++){
+			if (client[j].Montant > client[j + 1].Montant){
+				tab_temp=client[j];
+				client[j] =client[j + 1];
+				client[j + 1] = tab_temp;
+			}
+ 		}
+	}
+	printf("\n Les donners des comptes par ascendent est : \n\n  CIN  \t  Nom  \t  Prenom  \t  Montant \n");
+	for(i=0;i<3;i++){
+		client[i].Montant = client[i].Montant + ( client[i].Montant * 0,013 );
+		printf("  %s \t %s \t %s \t %lf \n \n",client[i].Nom,client[i].Prenom,client[i].CIN,client[i].Montant);
+	}
+}
+
+void Quitter_l_application(){
+	system("cls");
+    printf("\n\n\n\n \t cette application n est pas exister maintenant.");
+    exit(0);
 }
 
 
@@ -162,23 +196,47 @@ void Operation(){
 int main()
 {
     do {
-        //system("cls");
+        system("cls");
         Affichage_Menu(); //affich le menu.
         choix = choisir(choix);//choisir l'opération.
         switch (choix){
             case 1 : //crée un compte.
-                Introduire_un_compte_bancaire(CIN, Nom, Prenom, Montant);
-                printf("\n \n");
+                Introduire_un_compte_bancaire();
+                printf("\n \t Votre compte bancaire a ete cree avec succes : \n\n");
+                Affichage();
                 retour_a_menu();
                 break;
             case 2 : //crée plusieur compte.
-                Introduire_plusieurs_comptes_bancaires(CIN, Nom, Prenom, Montant);
-                printf("\n \n");
+                Introduire_plusieurs_comptes_bancaires();
                 retour_a_menu();
                 break;
-            case 3 : 
-                Operation();
+            case 3 :  //Operation.
+            	Operation();
+            	retour_a_menu();
                 break;
+            case 4 : //affichage ( Asc Desc recherche_par_cin ...).
+            	system("cls");
+            	printf("\n \t Choisir une methode d affichage : \n\n\n");
+            	printf(" 1--Affichage par odre ascendent : \n\n 2--Affichage par odre descendent : \n\n\n\n");
+            	choix = choisir(choix);
+            	switch (choix){
+            		case 1 :
+            			system("cls");
+            			Ascendent();
+            			retour_a_menu();
+            			break;
+            		case 2 :
+            			system("cls");
+            			Descendent();
+            			retour_a_menu();
+            			break;
+            	case 5 :
+            		Fidelisalton();
+            		break;
+            	case 6 :
+            		Quitter_l_application();
+            		break;
+				}
             default:
                 break;
         }
